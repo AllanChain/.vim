1f020b13d894827267470ad27f8f52311ebc4e49
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2019-07-14 09:37:51 +0800

    add upgrade in opt/

--- upgrade.sh
+++ upgrade.sh
@@ -9,3 +9,13 @@ for i in $base_dir/* ; do
         cd $base_dir
     fi
 done
+
+base_dir="$HOME/.vim/pack/dist/opt"
+cd $base_dir
+for i in $base_dir/* ; do
+    if [ -d "$i" ]; then
+        echo "$i"
+        cd $i && git pull origin master
+        cd $base_dir
+    fi
+done
